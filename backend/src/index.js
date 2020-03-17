37737d17c4829a0ace88d9848027c674792f183c
Mert Demirok <mertdemirok80@gmail.com>
2020-03-17 01:06:41 +0100

    install dotenv package, create express router

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+  });
+
+
+
