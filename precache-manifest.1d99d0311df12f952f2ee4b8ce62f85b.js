9264a206e0be05e18dec914d3164b14d3c289124
Mert Demirok <mertdemirok80@gmail.com>
2020-03-17 01:35:53 +0100

    Updates

---
+++ precache-manifest.1d99d0311df12f952f2ee4b8ce62f85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88788631d75d42b0fc407efa71e67900",
+    "url": "/DanceWithMe/index.html"
+  },
+  {
+    "revision": "ac33290e3e0fa45ce4fe",
+    "url": "/DanceWithMe/static/css/main.711391f2.chunk.css"
+  },
+  {
+    "revision": "ace57f294a2293dbf401",
+    "url": "/DanceWithMe/static/js/2.c5be568c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/DanceWithMe/static/js/2.c5be568c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac33290e3e0fa45ce4fe",
+    "url": "/DanceWithMe/static/js/main.f4a55291.chunk.js"
+  },
+  {
+    "revision": "a73a5aa45a84df63f21e",
+    "url": "/DanceWithMe/static/js/runtime-main.ab1e9e36.js"
+  }
+]);
