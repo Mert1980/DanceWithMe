b35b144df0b93725f16b687bde05c23ed1019318
Mert Demirok <mertdemirok80@gmail.com>
2020-03-18 23:08:59 +0100

    setup express route and mongoose connection

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,14 +1,14 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.ATLAS_URI, {
-//     useNewUrlParser:true,
-//     useUnifiedTopology: true,
-//     useFindAndModify:false,
-//     useCreateIndex: true // --> this allow as to access data quickly
-//   });
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useFindAndModify:false,
+    useCreateIndex: true // --> this allow as to access data quickly
+  });
 
-//   const connection = mongoose.connection;
+  const connection = mongoose.connection;
 
-//   connection.once('open', ()=>{
-//       console.log("MongoDB database connection established successfully!")
-//   })
+  connection.once('open', ()=>{
+      console.log("MongoDB database connection established successfully!")
+  })
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 // require('./db/mongoose');
 
 const app = express();
+
+const path = require('path');
+require('dotenv').config({path: path.resolve(process.cwd(), 'config', '.env'), debug: true});
+
 const port = process.env.PORT
 console.log(port)
 
