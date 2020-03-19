4839a6bece29339f704363848dd0f08acc4f4dc2
Mert Demirok <mertdemirok80@gmail.com>
2020-03-19 21:43:28 +0100

    set create profile and login routes, create user model

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -4,15 +4,24 @@ const User = require("../models/user");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
-    // creating new instance of User
-    // console.log(req.body);
-    const user = new User(req.body);
-      try {
-      await user.save();
-      res.status(201).send(user);
-    } catch (e) {
-      res.status(400).send(e.message);
-    }
-  });
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({user, token});
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email,req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({user, token});
+  } catch (e) {
+    res.status(400).send("Username or password is not correct");
+  }
+});
 
 module.exports = router;
