e6bc1355fb14837e82413008ad0a87bb32871292
Mert Demirok <mertdemirok80@gmail.com>
2020-03-19 14:09:52 +0100

    create a new user in the database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-
+const userRouter = require("./routers/user");
 require('./db/mongoose.js');
-// require('./db/mongodb')
 
 const app = express();
 app.use(express.json());
+app.use(userRouter);
 
 const path = require('path');
 require('dotenv').config({path: path.resolve(process.cwd(), 'config', '.env'), debug: true});
 
 const port = process.env.PORT
-console.log(process.env.PORT)
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
---
+++ backend/src/routers/user.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+    // creating new instance of User
+    // console.log(req.body);
+    const user = new User(req.body);
+      try {
+      await user.save();
+      res.status(201).send(user);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+module.exports = router;
