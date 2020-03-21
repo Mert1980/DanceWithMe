b4256eaadd2309a65690d8712c27803716118440
Mert Demirok <mertdemirok80@gmail.com>
2020-03-21 09:31:57 +0100

    delete dublicated components file, correct the file route names in due course

--- frontend/dance-with-me/src/App.js
+++ frontend/dance-with-me/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import './App.css';
-import Login from './Components/Login/Login'
-import SignUp from './Components/Sing-Up/SignUp'
+import Login from './components/Login/Login'
+import SignUp from './components/Signup/Signup'
 
 function App() {
   return (
