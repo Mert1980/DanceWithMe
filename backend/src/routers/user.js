51bb46a1a6c90a4fa8d05ff86b33fcdfc5370920
Mert Demirok <mertdemirok80@gmail.com>
2020-03-22 14:00:10 +0100

    set up email notification when the user signs up

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const User = require("../models/user");
-
 const router = new express.Router();
+const sendWelcomeEmail = require('../emails/account')
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({user, token});
   } catch (e) {
