71eb51c82505653e0546cac39f3f96069482824a
ilhan_koral <ilhan.koral@ttl.be>
2020-04-07 23:31:36 +0200

    given matched user from backend iterated and added to match cards
    stil user id is needed for filter out to looged user. This is first implementation

--- client/src/components/Profile/Match-users/ListMatchedUsers.jsx
+++ client/src/components/Profile/Match-users/ListMatchedUsers.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 
-function ListMatchedUsers (users) {
-    console.log(users)
-    return(
-        <div>
-            <ul>
-                <li>Name: {users.users[1].name}</li>
-                <li>Location:{users.users[1].location}</li>
-                <li>Dance Prefence: {users.users[1].dance_preference[0]}</li>
-                <li>Dance Prefence: {users.users[1].dance_preference[1]}</li>
-            </ul>
+function UserMatch(props) {
+    console.log(props.item)
+    const interest = props.item.dance_preference.join(', ');
 
+    return (
+
+        <div class="card-body">
+            <div class="container">
+                <div class="row mt-1">
+                    <div class="col-sm-3"><img src="../../../../public/images/img_avatar1.png" class="img-fluid" alt="e" /></div>
+                    <div class="col-sm-6">
+                        <h6 class="font-weight-light">{props.item.name}</h6>
+                        <small>{interest}</small>
+                        <p class="lead">{props.item.location}</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <button type="button" class="btn btn-outline-success mt-2"><small>+</small></button>
+                    </div>
+                </div>
+                <hr />
+            </div>
         </div>
-    ) 
+
+    );
 }
 
+function ListMatchedUsers (props) {
+    console.log(props.users)
+    const matchedUsers = props.users.map(item => <UserMatch item = {item}/>)
+    return(
+        <div id="collapseOne" class="collapse show">
+            {matchedUsers}
+        </div>
+    );
+}
 export default ListMatchedUsers
